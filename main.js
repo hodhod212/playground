2abda2f3e061de10094289ecd9d7b128822f5ff4
benjamin lolipop <hodhod212@yahoo.com>
2018-10-04 10:54:48 +0200

    ok

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 consule.log("It works")
-function changeBackground(color) {
+/*function changeBackground(color) {
     document.body.style.background = color;
+ } */
+ /*function changeBackground() {
+     var backgroundColor =  document.body.style.backgroundColor;
+   if( backgroundColor == "white") {
+       backgroundColor = "purple";
+   } else {
+       backgroundColor = "white";
+   }
+ }*/
+ function changeBackground() {
+     if (document.body.style.backgroundColor == "white") {
+         document.body.style.backgroundColor = "purple"
+     } else {
+        document.body.style.backgroundColor = "white"
+     }
  }
+
+ var fotballplayer = {
+     name:"David Jensen",
+     age:22,
+     shirtNumber: 14,
+     position: "Mid-fielder",
+     gender: "male"
+ };
+ var newFotballPlayer =fotballplayer;
+ newFotballPlayer.name = "Albert Eistein";
+ consule.log(newFotballPlayer);
+ consule.log(fotballPlayer);
